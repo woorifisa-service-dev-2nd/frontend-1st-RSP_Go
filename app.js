cc7881a066d0815c8daecec88b9627a46551616c
yueunkim <yueunkim0605@gmail.com>
2023-11-26 16:50:08 +0900

    css, js 수정

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 /**
- * 처음 사람 이미지 
+ * 처음 사람 이미지
  */
 imgSrcArray = [
-    "https://img.icons8.com/ios/250/000000/user-male.png",
-    "https://img.icons8.com/ios/250/000000/businessman.png",
-    "https://img.icons8.com/ios/250/000000/user.png",
-    "https://img.icons8.com/ios/250/000000/user-female.png",
-    "https://img.icons8.com/ios/250/000000/air-jordan.png",
-    "https://img.icons8.com/ios/250/000000/reddit.png",
-    "https://img.icons8.com/ios/250/000000/twitter.png",
-    "https://img.icons8.com/ios-filled/250/000000/mac-os.png",
-    "https://img.icons8.com/ios/250/000000/facebook-new.png",
-    "https://img.icons8.com/ios/250/000000/google-logo.png",
-    "https://img.icons8.com/ios/250/000000/instagram-new.png"
+  "https://img.icons8.com/ios/250/000000/user-male.png",
+  "https://img.icons8.com/ios/250/000000/businessman.png",
+  "https://img.icons8.com/ios/250/000000/user.png",
+  "https://img.icons8.com/ios/250/000000/user-female.png",
+  "https://img.icons8.com/ios/250/000000/air-jordan.png",
+  "https://img.icons8.com/ios/250/000000/reddit.png",
+  "https://img.icons8.com/ios/250/000000/twitter.png",
+  "https://img.icons8.com/ios-filled/250/000000/mac-os.png",
+  "https://img.icons8.com/ios/250/000000/facebook-new.png",
+  "https://img.icons8.com/ios/250/000000/google-logo.png",
+  "https://img.icons8.com/ios/250/000000/instagram-new.png",
 ];
-for(let i = 0; i <2; i++){
-    let tagArea = document.getElementById('people');
-    let newTag = document.createElement('div');
-    newTag.setAttribute('class', 'person');
-    newTag.innerHTML = `
+for (let i = 0; i < 2; i++) {
+  let tagArea = document.getElementById("people");
+  let newTag = document.createElement("div");
+  newTag.setAttribute("class", "person");
+  newTag.innerHTML = `
     <div class="people-img">
         <img src=${imgSrcArray[Math.floor(Math.random() * imgSrcArray.length)]}>
     </div>
@@ -28,35 +28,38 @@ for(let i = 0; i <2; i++){
         <li> <button class="selected">보</button></li>
     </ul>
                         `;
-    tagArea.appendChild(newTag);
-    const div = document.getElementsByClassName('person');
+  tagArea.appendChild(newTag);
+  const div = document.getElementsByClassName("person");
 }
 /**
  * 사용자 가위 바위 보 정보 가져오기 default 0(가위)
  */
 let human = 2; // 참가자 수
-let pelpleRspArray =[];
-function getRSP(){
-    const selectsArray = document.querySelectorAll('.select-box');
-    selectsArray.forEach((selects, idx) => {
-        selects.querySelectorAll(".selected").forEach((selected,value) => {
-            selected.addEventListener("click",()=>{pelpleRspArray[idx] = value;
-            console.log(pelpleRspArray);})
-        });
+let pelpleRspArray = [];
+function getRSP() {
+  const selectsArray = document.querySelectorAll(".select-box");
+  selectsArray.forEach((selects, idx) => {
+    selects.querySelectorAll(".selected").forEach((selected, value) => {
+      selected.addEventListener("click", () => {
+        pelpleRspArray[idx] = value;
+        console.log(pelpleRspArray);
+      });
     });
+  });
 }
-getRSP()
+getRSP();
 
-let addButton = document.getElementById('addPerson');
-let removeButton = document.getElementById('removePerson');
+let addButton = document.getElementById("addPerson");
+let removeButton = document.getElementById("removePerson");
 /**
  * 사람 추가 기능
  */
-addButton.addEventListener('click', () => {
-    let tagArea = document.getElementById('people');
-    let newTag = document.createElement('div');
-    newTag.setAttribute('class', 'person');
-    newTag.innerHTML = `
+addButton.addEventListener("click", () => {
+  if (human >= 4) return; // 사람 수가 4명 이상이면 추가되지 않도록
+  let tagArea = document.getElementById("people");
+  let newTag = document.createElement("div");
+  newTag.setAttribute("class", "person");
+  newTag.innerHTML = `
     <div class="people-img">
         <img src=${imgSrcArray[Math.floor(Math.random() * imgSrcArray.length)]}>
     </div>
@@ -66,91 +69,102 @@ addButton.addEventListener('click', () => {
         <li> <button class="selected">보</button></li>
     </ul>
                         `;
-    tagArea.appendChild(newTag);
-    const div = document.getElementsByClassName('person');
-    human++;
-    if (human > 2) {
-        removeButton.removeAttribute('disabled');
-    }
-    getRSP()
+  tagArea.appendChild(newTag);
+  const div = document.getElementsByClassName("person");
+  human++;
+  if (human > 2) {
+    removeButton.removeAttribute("disabled");
+  }
+  getRSP();
 });
 /**
  * 사람 삭제 기능
  */
-removeButton.addEventListener('click', () => {
-    if(human == 2) return;
-    const div = document.getElementsByClassName('person');
-    const div2 = div[div.length - 1];
-    div2.remove();
-    human--;
-    if (human === 2) {
-        removeButton.setAttribute('disabled', '');
-    }
-    getRSP()
+removeButton.addEventListener("click", () => {
+  if (human == 2) return;
+  const div = document.getElementsByClassName("person");
+  const div2 = div[div.length - 1];
+  div2.remove();
+  human--;
+  if (human === 2) {
+    removeButton.setAttribute("disabled", "");
+  }
+  getRSP();
 });
 
-
 // pelpleRspArray[idx] = value
-
 
 /**
  * 결과 보기 기능
  */
-let resultButton = document.getElementById('resultBtn');
-resultButton.addEventListener('click', () => {
-    let winner = [];
-    let result = ''; // 결과 출력 값
-    for(let i = 0; i <human; i++){
-        if(typeof pelpleRspArray[i] === 'undefined'){ pelpleRspArray[i] = 0}
+let resultButton = document.getElementById("resultBtn");
+resultButton.addEventListener("click", () => {
+  let winner = [];
+  let result = ""; // 결과 출력 값
+  let notSelected = []; // 선택하지 않은 사람 번호를 저장할 배열
+
+  for (let i = 0; i < human; i++) {
+    if (typeof pelpleRspArray[i] === "undefined") {
+      pelpleRspArray[i] = 0;
+      notSelected.push(i + 1); // 선택하지 않은 사람 번호 추가
     }
+  }
+
+  if (notSelected.length > 0) {
+    result =
+      notSelected.join(", ") + "번 사람이 가위바위보를 선택하지 않았습니다.";
+  } else {
     console.log(pelpleRspArray.slice(0, human));
     let rspSet = new Set(pelpleRspArray.slice(0, human));
-    rspSet = [...rspSet]
-    if(rspSet.length === 2){
-        let victory = rspLogic(rspSet[0], rspSet[1])
-        for(let i = 0; i <human; i++){
-            if(pelpleRspArray[i] == victory){winner.push(i + 1)}
+    rspSet = [...rspSet];
+    if (rspSet.length === 2) {
+      let victory = rspLogic(rspSet[0], rspSet[1]);
+      for (let i = 0; i < human; i++) {
+        if (pelpleRspArray[i] == victory) {
+          winner.push(i + 1);
         }
-        winner.forEach((win,idx) => {
-            result += win
-            if(idx + 1 == winner.length){
-                if(winner.length == 1){
-                    result += " 번 사람이 승리했습니다."
-                }else{
-                    result += " 번 사람들이 승리했습니다."
-                }
-            }else{
-                result += ", "
-            }
-        });
-    }else if(rspSet.length === 1){
-        result = '무승부!!'
-    }else{
-        result = '다시!!'
+      }
+      winner.forEach((win, idx) => {
+        result += win;
+        if (idx + 1 == winner.length) {
+          if (winner.length == 1) {
+            result += " 번 사람이 승리했습니다.";
+          } else {
+            result += " 번 사람들이 승리했습니다.";
+          }
+        } else {
+          result += ", ";
+        }
+      });
+    } else if (rspSet.length === 1) {
+      result = "무승부!!";
+    } else {
+      result = "다시!!";
     }
-    const resultText = document.getElementById('result-text');
-    resultText.innerHTML = result;
+  }
+  const resultText = document.getElementById("result-text");
+  resultText.innerHTML = result;
 });
 
-function rspLogic(a, b){
-    // 가위 : 0 / 바위 : 1 / 보 : 2
-    // 가위 < 바위
-    if((a == 0 || a == 1) && (b == 0 || b == 1)){
-        return 1;
-    }
-    // 바위 < 보
-    if((a == 1 || a == 2) && (b == 1 || b == 2)){
-        return 2
-    }
-    // 보 < 가위
-    if((a == 0 || a == 2) && (b == 0 || b == 2)){
-        return 0;
-    }
+function rspLogic(a, b) {
+  // 가위 : 0 / 바위 : 1 / 보 : 2
+  // 가위 < 바위
+  if ((a == 0 || a == 1) && (b == 0 || b == 1)) {
+    return 1;
+  }
+  // 바위 < 보
+  if ((a == 1 || a == 2) && (b == 1 || b == 2)) {
+    return 2;
+  }
+  // 보 < 가위
+  if ((a == 0 || a == 2) && (b == 0 || b == 2)) {
+    return 0;
+  }
 }
 /**
  * 다시하기 기능
-*/
-let resetButton = document.getElementById('resetBtn');
-resetButton.addEventListener('click', () => {
-    location.reload();
+ */
+let resetButton = document.getElementById("resetBtn");
+resetButton.addEventListener("click", () => {
+  location.reload();
 });
