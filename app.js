7cf4d06918bd7f50343f699c57d18921b6db4d9a
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-27 11:01:52 +0900

    refactor: js

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
 /**
- * 처음 사람 이미지
+ * 사용자 가위 바위 보 정보 가져오기
  */
-imgSrcArray = [
-  "https://img.icons8.com/ios/250/000000/user-male.png",
-  "https://img.icons8.com/ios/250/000000/businessman.png",
-  "https://img.icons8.com/ios/250/000000/user.png",
-  "https://img.icons8.com/ios/250/000000/user-female.png",
-  "https://img.icons8.com/ios/250/000000/air-jordan.png",
-  "https://img.icons8.com/ios/250/000000/reddit.png",
-  "https://img.icons8.com/ios/250/000000/twitter.png",
-  "https://img.icons8.com/ios-filled/250/000000/mac-os.png",
-  "https://img.icons8.com/ios/250/000000/facebook-new.png",
-  "https://img.icons8.com/ios/250/000000/google-logo.png",
-  "https://img.icons8.com/ios/250/000000/instagram-new.png",
-];
-for (let i = 0; i < 2; i++) {
-  let tagArea = document.getElementById("people");
-  let newTag = document.createElement("div");
-  newTag.setAttribute("class", "person");
-  newTag.innerHTML = `
-    <div class="people-img">
-        <img src=${imgSrcArray[Math.floor(Math.random() * imgSrcArray.length)]}>
-    </div>
-    <ul class="container flex-direction-column select-box">
-        <li> <button class="selected">가위 </button></li>
-        <li> <button class="selected">바위</button></li>
-        <li> <button class="selected">보</button></li>
-    </ul>
-                        `;
-  tagArea.appendChild(newTag);
-  const div = document.getElementsByClassName("person");
-}
-/**
- * 사용자 가위 바위 보 정보 가져오기 default 0(가위)
- */
-let human = 2; // 참가자 수
-let peopleRspArray = [];
 function getRSP() {
   const selectsArray = document.querySelectorAll(".select-box");
   selectsArray.forEach((selects, idx) => {
@@ -46,13 +11,69 @@ function getRSP() {
     });
   });
 }
+
+function rspLogic(a, b) {
+  // 가위 : 0 / 바위 : 1 / 보 : 2
+  // 가위 < 바위
+  if ((a == 0 || a == 1) && (b == 0 || b == 1)) {
+    return 1;
+  }
+  // 바위 < 보
+  if ((a == 1 || a == 2) && (b == 1 || b == 2)) {
+    return 2;
+  }
+  // 보 < 가위
+  if ((a == 0 || a == 2) && (b == 0 || b == 2)) {
+    return 0;
+  }
+}
+
+function init(){
+  /**
+   * 처음 사람 이미지
+   */
+  imgSrcArray = [
+    "https://img.icons8.com/ios/250/000000/user-male.png",
+    "https://img.icons8.com/ios/250/000000/businessman.png",
+    "https://img.icons8.com/ios/250/000000/user.png",
+    "https://img.icons8.com/ios/250/000000/user-female.png",
+    "https://img.icons8.com/ios/250/000000/air-jordan.png",
+    "https://img.icons8.com/ios/250/000000/reddit.png",
+    "https://img.icons8.com/ios/250/000000/twitter.png",
+    "https://img.icons8.com/ios-filled/250/000000/mac-os.png",
+    "https://img.icons8.com/ios/250/000000/facebook-new.png",
+    "https://img.icons8.com/ios/250/000000/google-logo.png",
+    "https://img.icons8.com/ios/250/000000/instagram-new.png",
+  ];
+  for (let i = 0; i < 2; i++) {
+    let tagArea = document.getElementById("people");
+    let newTag = document.createElement("div");
+    newTag.setAttribute("class", "person");
+    newTag.innerHTML = `
+      <div class="people-img">
+          <img src=${imgSrcArray[Math.floor(Math.random() * imgSrcArray.length)]}>
+      </div>
+      <ul class="container flex-direction-column select-box">
+          <li> <button class="selected">가위 </button></li>
+          <li> <button class="selected">바위</button></li>
+          <li> <button class="selected">보</button></li>
+      </ul>
+                          `;
+    tagArea.appendChild(newTag);
+    const div = document.getElementsByClassName("person");
+  }
+}
+
+init();
 getRSP();
 
 let addButton = document.getElementById("addPerson");
 let removeButton = document.getElementById("removePerson");
+let human = 2; // 참가자 수
+let peopleRspArray = [];
 /**
- * 사람 추가 기능
- */
+   * 사람 추가 기능
+   */
 addButton.addEventListener("click", () => {
   if (human >= 4) return; // 사람 수가 4명 이상이면 추가되지 않도록
   let tagArea = document.getElementById("people");
@@ -83,6 +104,7 @@ removeButton.addEventListener("click", () => {
   if (human == 2) return;
   const div = document.getElementsByClassName("person");
   const div2 = div[div.length - 1];
+  peopleRspArray.pop();
   div2.remove();
   human--;
   if (human === 2) {
@@ -102,7 +124,7 @@ resultButton.addEventListener("click", () => {
 
   for (let i = 0; i < human; i++) {
     if (typeof peopleRspArray[i] === "undefined") {
-      peopleRspArray[i] = 0;
+      // peopleRspArray[i] = 0;
       notSelected.push(i + 1); // 선택하지 않은 사람 번호 추가
     }
   }
@@ -142,21 +164,6 @@ resultButton.addEventListener("click", () => {
   resultText.innerHTML = result;
 });
 
-function rspLogic(a, b) {
-  // 가위 : 0 / 바위 : 1 / 보 : 2
-  // 가위 < 바위
-  if ((a == 0 || a == 1) && (b == 0 || b == 1)) {
-    return 1;
-  }
-  // 바위 < 보
-  if ((a == 1 || a == 2) && (b == 1 || b == 2)) {
-    return 2;
-  }
-  // 보 < 가위
-  if ((a == 0 || a == 2) && (b == 0 || b == 2)) {
-    return 0;
-  }
-}
 /**
  * 다시하기 기능
  */
